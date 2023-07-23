0c20fba452c3113c5975324f81b168506491521b
Sihyeon Joo <sihyeonjoo@Sihyeonui-MacBookAir.local>
2023-07-23 22:20:59 +0900

    [MATDORI-54] feat:Title컴포넌트 생성

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import NavigationBar from "@/components/NavigationBar";
 import { RecoilRoot } from "recoil";
 import Review from "@/components/Review";
 import Input from "@/components/Input";
+import BigTitle from "@/components/Title/BigTitle";
+import SmallTitle from "@/components/Title/SmallTitle";
 
 const Home = () => {
   return (
@@ -12,8 +14,9 @@ const Home = () => {
       <Review review={{title:"미련한 컴공", content:"이건 댓글. 니가 뭔데 가메이를 혼자 알려고 함? ㅋㅋㅋ", writeDay:"23/04/20 22:22", heartCount:5}}/>
       <Review review={{title:"미련한 컴공", content:"이건 댓글. 니가 뭔데 가메이를 혼자 알려고 함? ㅋㅋㅋ", writeDay:"23/04/20 22:22", heartCount:5}}/>
       <Review review={{title:"미련한 컴공", content:"이건 댓글. 니가 뭔데 가메이를 혼자 알려고 함? ㅋㅋㅋ", writeDay:"23/04/20 22:22", heartCount:5}}/>
-      
-      
+      <Input info={{width:"500px",height:"50px",placeHolder:"placeHolder 설정"}}/>
+      <BigTitle>컴퓨터공학과의 맛도리</BigTitle>
+      <SmallTitle>맛도리에게 추천받기</SmallTitle>
     </>
   )
 }
