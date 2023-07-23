3b76e77c0f170725b3a2d7bc3340c087e5b1a429
Sihyeon Joo <sihyeonjoo@Sihyeonui-MacBookAir.local>
2023-07-23 21:57:55 +0900

    [MATDORI-54] feat:Input 컴포넌트 생성

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import NavigationBar from "@/components/NavigationBar";
 import { RecoilRoot } from "recoil";
 import Review from "@/components/Review";
+import Input from "@/components/Input";
 
 const Home = () => {
   return (
@@ -11,6 +12,8 @@ const Home = () => {
       <Review review={{title:"미련한 컴공", content:"이건 댓글. 니가 뭔데 가메이를 혼자 알려고 함? ㅋㅋㅋ", writeDay:"23/04/20 22:22", heartCount:5}}/>
       <Review review={{title:"미련한 컴공", content:"이건 댓글. 니가 뭔데 가메이를 혼자 알려고 함? ㅋㅋㅋ", writeDay:"23/04/20 22:22", heartCount:5}}/>
       <Review review={{title:"미련한 컴공", content:"이건 댓글. 니가 뭔데 가메이를 혼자 알려고 함? ㅋㅋㅋ", writeDay:"23/04/20 22:22", heartCount:5}}/>
+      
+      
     </>
   )
 }
