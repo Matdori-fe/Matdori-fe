3619889a6f7f3c33dc7f3aafed3f58cc38ab9e78
pakxe <pigkill40@naver.com>
2023-07-24 14:42:54 +0900

    [MATDORI-87] feat: RoundButton Story 작성

---
+++ stories/hi/RoundButton.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import RoundButton from '../../components/RoundButton/RoundButton';
+
+const meta: Meta<typeof RoundButton> = {
+	title: 'Test/RoundButton',
+	component: RoundButton,
+};
+
+export default meta;
+type Story = StoryObj<typeof RoundButton>;
+
+export const Eng: Story = {
+	args: {
+		label: 'sehyun',
+	},
+};
+
+export const Kor: Story = {
+	args: {
+		label: '세현',
+	},
+};
