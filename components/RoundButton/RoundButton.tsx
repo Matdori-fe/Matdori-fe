d1eeceffd63702adff6d6a9a3d75345bf9fbe2ac
pakxe <pigkill40@naver.com>
2023-07-28 18:17:40 +0900

    [MATDORI-87] feat: RoundButton에 onClick prop 작성

--- components/RoundButton/RoundButton.tsx
+++ components/RoundButton/RoundButton.tsx
@@ -2,14 +2,18 @@ import Text from '../Text/Text';
 
 interface ButtonProps {
 	label: string;
+	onClick: () => void;
 }
 
-export default function RoundButton({ label }: ButtonProps) {
+export default function RoundButton({ label, onClick }: ButtonProps) {
 	return (
-		<div className='px-[10px] py-[5px] bg-white rounded-2xl border border-lightGray justify-center items-center inline-flex'>
+		<button
+			onClick={onClick}
+			className='px-[10px] py-[5px] bg-white rounded-2xl border border-lightGray justify-center items-center inline-flex'
+		>
 			<Text color='darkGray' size='xxs' fontWeight='normal'>
 				{label}
 			</Text>
-		</div>
+		</button>
 	);
 }
