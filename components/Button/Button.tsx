31ced1d3ee8d22967b097684a25fa3c45d3814fa
pakxe <pigkill40@naver.com>
2023-07-29 00:40:16 +0900

    [MATDORI-87] feat: Button 컴포넌트 완성

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,29 +1,51 @@
-import type { FC } from 'react';
+'use client';
+
+import Text from '../Text/Text';
+import Link from 'next/link';
+import { RiEdit2Fill } from 'react-icons/ri';
+import Toast from '../Toast/Toast';
 
 interface ButtonProps {
-	name: string;
-	className: string;
+	label: string;
+	variant: 'active' | 'inactive';
+	writeIcon?: boolean;
+	href?: string;
+	errorMessage: string;
 }
 
-const Button = ({ name, className }: ButtonProps) => {
+const BASE_BUTTON_CLASSES =
+	'fixed bottom-0 md:w-[calc(768px-40px)] h-[60px] w-[calc(100%-40px)] py-3.5 justify-center items-center rounded-basic inline-flex my-[15px] mx-[20px]';
+
+const variantClass = {
+	active: 'bg-100',
+	inactive: 'bg-gray',
+};
+
+const Button = ({
+	label,
+	variant = 'active',
+	writeIcon,
+	href = '',
+	errorMessage,
+}: ButtonProps) => {
 	return (
-		<button
-			className={`p-2 rounded-lg shadow-lg hover:shadow font-bold ${className}`}
+		<Link
+			href={href}
+			onClick={variant === 'inactive' ? () => Toast(errorMessage) : undefined}
 		>
-			{name}
-		</button>
+			<button className={`${BASE_BUTTON_CLASSES} ${variantClass[variant]}`}>
+				<div className='flex items-center gap-[8px]'>
+					{writeIcon && <RiEdit2Fill fill='white' size='20' />}
+					<Text color='white' fontWeight='bold' size='lg'>
+						{label}
+					</Text>
+				</div>
+			</button>
+		</Link>
 	);
 };
 
 export default Button;
 
-// import tw from 'tailwind-styled-components';
-
-// const StyledButton = tw.button`
-//   bg-100
-//   text-xl
-// `;
-
-// export default function Button() {
-// 	return <StyledButton>hi</StyledButton>;
-// }
+// active, inactive
+// TODO: onClick을 어떻게 넣을지 아니면 Link로 href만 사용할지..
