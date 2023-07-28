8c8e52d20b3fd80de53a1d3f130a71eee10dba08
pakxe <pigkill40@naver.com>
2023-07-29 00:41:44 +0900

    [MATDORI-87] chore: storybook 재설치

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,30 +1,22 @@
-import type { StorybookConfig } from '@storybook/nextjs';
-const config: StorybookConfig = {
-	stories: [
-		'../stories/**/*.mdx',
-		'../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-	],
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-onboarding',
-		'@storybook/addon-interactions',
-		{
-			name: '@storybook/addon-post',
-			options: {
+import type { StorybookConfig } from "@storybook/nextjs";
 
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-		},
-	],
-	framework: {
-		name: '@storybook/nextjs',
-		options: {},
-	},
-	docs: {
-		autodocs: 'tag',
-	},
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
 };
 export default config;
