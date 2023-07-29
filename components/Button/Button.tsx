0a087160339bd264d133ea405b6bb92ee9faa952
pakxe <pigkill40@naver.com>
2023-07-30 03:29:01 +0900

    [MATDORI-87] style: 버튼이 모달 안에 있을 때는 absolute 로 바뀜

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -4,17 +4,19 @@ import Text from '../Text/Text';
 import Link from 'next/link';
 import { RiEdit2Fill } from 'react-icons/ri';
 import Toast from '../Toast/Toast';
+import { useRouter } from 'next/navigation';
 
 interface ButtonProps {
 	label: string;
 	variant: 'active' | 'inactive';
 	writeIcon?: boolean;
 	href?: string;
-	errorMessage: string;
+	errorMessage?: string;
+	modal: boolean;
 }
 
 const BASE_BUTTON_CLASSES =
-	'fixed bottom-0 md:w-[calc(768px-40px)] h-[60px] w-[calc(100%-40px)] py-3.5 justify-center items-center rounded-basic inline-flex my-[15px] mx-[20px]';
+	'md:w-[calc(768px-40px)] h-[60px] w-[calc(100%-40px)] py-3.5 justify-center items-center rounded-basic inline-flex my-[15px] mx-[20px]';
 
 const variantClass = {
 	active: 'bg-100',
@@ -26,26 +28,30 @@ const Button = ({
 	variant = 'active',
 	writeIcon,
 	href = '',
-	errorMessage,
+	errorMessage = '',
+	modal = false,
 }: ButtonProps) => {
+	const router = useRouter();
+
 	return (
-		<Link
-			href={href}
-			onClick={variant === 'inactive' ? () => Toast(errorMessage) : undefined}
+		<button
+			onClick={
+				variant === 'inactive'
+					? () => Toast(errorMessage)
+					: () => router.push(href)
+			}
+			className={`${BASE_BUTTON_CLASSES} ${variantClass[variant]} ${
+				modal ? 'mx-0 w-full absolute bottom-0' : 'fixed bottom-0'
+			}`}
 		>
-			<button className={`${BASE_BUTTON_CLASSES} ${variantClass[variant]}`}>
-				<div className='flex items-center gap-[8px]'>
-					{writeIcon && <RiEdit2Fill fill='white' size='20' />}
-					<Text color='white' fontWeight='bold' size='lg'>
-						{label}
-					</Text>
-				</div>
-			</button>
-		</Link>
+			<div className='flex items-center gap-[8px]'>
+				{writeIcon && <RiEdit2Fill fill='white' size='20' />}
+				<Text color='white' fontWeight='bold' size='lg'>
+					{label}
+				</Text>
+			</div>
+		</button>
 	);
 };
 
 export default Button;
-
-// active, inactive
-// TODO: onClick을 어떻게 넣을지 아니면 Link로 href만 사용할지..
