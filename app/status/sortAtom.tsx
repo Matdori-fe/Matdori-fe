f21983fd5215f4531282105354b0046c42a5741f
pakxe <pigkill40@naver.com>
2023-07-30 02:08:14 +0900

    [MATDORI-87] feat: 선택된 정렬 상태를 저장할 sortAtom 완성

--- app/status/sortAtom.tsx
+++ app/status/sortAtom.tsx
@@ -1,6 +1,12 @@
 import { atom } from 'recoil';
 
+// FIXME: 리펙토링 해야함
 export const SortListAtom = atom<string[]>({
 	key: 'sortList',
 	default: ['별점', '음식 맛', '가성비', '청결'],
+});
+
+export const SelectedSortAtom = atom<boolean[]>({
+	key: 'SelectedSort',
+	default: [true, false, false, false],
 });
