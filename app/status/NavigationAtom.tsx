987efd0b18b2f14392e59d18aac33801b3a4e416
sean <sean2337@naver.com>
2023-07-31 14:00:10 +0900

    [MATDORI-54] feature: recoilPersist 기능 추가

--- app/status/NavigationAtom.tsx
+++ app/status/NavigationAtom.tsx
@@ -1,7 +1,17 @@
-'use client'
+"use client";
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const sessionStorage =
+  typeof window !== "undefined" ? window.sessionStorage : undefined;
+
+const { persistAtom } = recoilPersist({
+  key: "NavigationAtom",
+  storage: sessionStorage,
+});
 
 export const NavigationAtom = atom<boolean[]>({
-    key:'NavigationAtom',
-    default:[false,false,false,false]
-})
+  key: "NavigationAtom",
+  default: [false, false, false, false],
+  effects_UNSTABLE: [persistAtom],
+});
