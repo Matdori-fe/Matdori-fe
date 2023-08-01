bc958326e527220c631baab26e279ba3663a7ead
sean <sean2337@naver.com>
2023-08-02 00:10:26 +0900

    [MATDORI-54] design: SmallButton 테두리 CSS 수정

--- components/Button/SmallButton.tsx
+++ components/Button/SmallButton.tsx
@@ -28,7 +28,7 @@ const SmallButtonBar: React.FC<{ type: ButtonType }> = (props) => {
     return (
       <>
         {/*가게 추천받기 white버전*/}
-        <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center border-lightgray border-[1px]">
+        <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center border-lightGray border-[1px]">
           <img src={`recommendImg.svg`} />
           <p className="font-Regular text-[12px] text-darkgray">
             가게 추천받기
@@ -52,7 +52,7 @@ const SmallButtonBar: React.FC<{ type: ButtonType }> = (props) => {
     return (
       <>
         {/*메뉴 추천받기 white버전*/}
-        <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center justify-between border-lightgray border-[1px] overflow-hidden">
+        <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center justify-between border-lightGray border-[1px] overflow-hidden">
           <p className="font-Regular text-[12px] text-darkgray ml-5">
             메뉴 추천받기
           </p>
@@ -65,7 +65,7 @@ const SmallButtonBar: React.FC<{ type: ButtonType }> = (props) => {
     return (
       <>
         {/*내 좋아요 컴포넌트*/}
-        <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center justify-between border-lightgray border-[1px] px-5">
+        <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center justify-between border-lightGray border-[1px] px-5">
           <img src={`heartWoman.svg`} className="h-[36px] mt-1.4" />
           <p className="font-Regular text-[12px] text-darkgray">내 좋아요</p>
         </div>
@@ -76,7 +76,7 @@ const SmallButtonBar: React.FC<{ type: ButtonType }> = (props) => {
     return (
       <>
         {/*내 활동 컴포넌트*/}
-        <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center justify-between border-lightgray border-[1px] px-5">
+        <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center justify-between border-lightGray border-[1px] px-5">
           <p className="font-Regular text-[12px] text-darkgray">내 활동</p>
           <img src={`smartPhone.svg`} className="h-[36px] mt-1.4" />
         </div>
