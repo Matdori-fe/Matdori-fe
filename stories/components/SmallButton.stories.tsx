9ac9b633d7b62dd3dde3b9471b01a7a7052f6629
sean <sean2337@naver.com>
2023-08-02 00:24:12 +0900

    [MATDORI-54] design: SmallButton CSS 완료

--- stories/components/SmallButton.stories.tsx
+++ stories/components/SmallButton.stories.tsx
@@ -37,7 +37,7 @@ const SmallButtonBar: React.FC<{ type: ButtonType }> = (props) => {
         {/*가게 추천받기 white버전*/}
         <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center border-lightGray border-[1px]">
           <RecommendSVG />
-          <p className="font-Regular text-[12px] text-darkgray">
+          <p className="font-Regular text-[12px] text-darkGray">
             가게 추천받기
           </p>
         </div>
@@ -60,7 +60,7 @@ const SmallButtonBar: React.FC<{ type: ButtonType }> = (props) => {
       <>
         {/*메뉴 추천받기 white버전*/}
         <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center justify-between border-lightGray border-[1px] overflow-hidden">
-          <p className="font-Regular text-[12px] text-darkgray ml-5">
+          <p className="font-Regular text-[12px] text-darkGray ml-5">
             메뉴 추천받기
           </p>
           <FoodBar />
@@ -74,7 +74,7 @@ const SmallButtonBar: React.FC<{ type: ButtonType }> = (props) => {
         {/*내 좋아요 컴포넌트*/}
         <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center justify-between border-lightGray border-[1px] px-5">
           <HeartWoman />
-          <p className="font-Regular text-[12px] text-darkgray">내 좋아요</p>
+          <p className="font-Regular text-[12px] text-darkGray">내 좋아요</p>
         </div>
       </>
     );
@@ -84,7 +84,7 @@ const SmallButtonBar: React.FC<{ type: ButtonType }> = (props) => {
       <>
         {/*내 활동 컴포넌트*/}
         <div className="w-[155px] h-[40px] bg-white rounded-[15px] flex items-center justify-between border-lightGray border-[1px] px-5">
-          <p className="font-Regular text-[12px] text-darkgray">내 활동</p>
+          <p className="font-Regular text-[12px] text-darkGray">내 활동</p>
           <SmartPhone />
         </div>
       </>
