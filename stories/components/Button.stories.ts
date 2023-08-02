54b468b8e42ff9d9555973f90f8d0329fd2a2fff
pakxe <pigkill40@naver.com>
2023-08-03 00:42:37 +0900

    [MATDORI-87] chore: props에 주석다는 방법 메모

--- stories/components/Button.stories.ts
+++ stories/components/Button.stories.ts
@@ -5,6 +5,18 @@ const meta: Meta<typeof Button> = {
 	title: 'Test/Button',
 	component: Button,
 	tags: ['autodocs'],
+	parameters: {
+		componentSubtitle: '버튼입니다',
+	},
+	// NOTE: storybook 주석 문서화
+	// argTypes: {
+	// 	label: {
+	// 		description: '여백의 크기를 설정합니다.',
+	// 		table: {
+	// 			type: { summary: 'SpacingVariant' },
+	// 		},
+	// 	},
+	// },
 };
 
 export default meta;
@@ -12,6 +24,7 @@ type Story = StoryObj<typeof Button>;
 
 export const Active: Story = {
 	args: {
+		/** 버튼 안에 들어갈 문장 */
 		label: '나도 족보 작성하기',
 		variant: 'active',
 		writeIcon: true,
