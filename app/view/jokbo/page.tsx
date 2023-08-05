3d9ad5d95461fe57e4b052283329e8fc99fd1852
sean <sean2337@naver.com>
2023-08-05 22:15:19 +0900

    [MATDORI-60] feature: 학과별 맛도리 생성

--- app/view/jokbo/page.tsx
+++ app/view/jokbo/page.tsx
@@ -1,11 +1,13 @@
 import JokboTitle from "./components/JokboTitle";
 import JokboCountBox from "./components/JokboCountBox";
+import DepartMentRecommened from "./components/DepartmentRecommend";
 
 const Jokbo = () => {
   return (
     <div className="w-full px-[20px]">
       <JokboTitle />
       <JokboCountBox />
+      <DepartMentRecommened />
     </div>
   );
 };
