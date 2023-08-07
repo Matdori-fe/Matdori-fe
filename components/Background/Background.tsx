cdd3d97ff7f1f44cb085a3226133031d41940e48
pakxe <pigkill40@naver.com>
2023-08-08 02:09:55 +0900

    [MATDORI-56] refactor: 모달을 route 기반에서 state 기반으로 리펙토링

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -1,15 +1,15 @@
 export default function Background({
 	className,
 	children,
-	onClick,
+	onClose,
 }: {
 	className?: string;
 	children?: React.ReactNode;
-	onClick?: () => void;
+	onClose?: () => void;
 }) {
 	return (
 		<div
-			onClick={onClick}
+			onClick={onClose}
 			className={`fixed w-screen h-full bg-transparent z-1 top-0 left-0 ${className} flex items-end`}
 		>
 			{children}
--- components/Modal/DepartmentModal.tsx
+++ components/Modal/DepartmentModal.tsx
@@ -1,59 +1,50 @@
 'use client';
 
-import { AnimatePresence } from 'framer-motion';
 import ModalLayout from './ModalLayout';
-import { SelectedSortAtom, SortListAtom } from '@/app/status/SortAtom';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { useRouter } from 'next/navigation';
+import { useRecoilState } from 'recoil';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useModal } from '@/hooks/useModal';
+import { DepartmentAtom } from '@/app/status/DepartmentAtom';
 
 // TODO: 선택된 과를 어디에 저장해서 사용할지 생각해야 한다. 선택된건 빨간색으로 표시하고 화면에도 띄울 것이기 때문임
-export default function DepartmentModal({
-	showModal,
-	href,
-}: {
-	showModal: boolean;
-	href: string;
-}) {
-	const router = useRouter();
-	const sortList = useRecoilValue(SortListAtom);
-	const [selectedSort, setSelectedSort] = useRecoilState(SelectedSortAtom);
+export default function DepartmentModal() {
+	const [departmentList, setDepartmentList] = useState([]);
 
-	// FIXME: 리펙토링. 하나의 관리하는 모듈을 만드는 것이 좋을 듯
-	const handleSelectSort = (i: number): boolean[] => {
-		const falseArr = new Array(4).fill(false);
-		falseArr[i] = true;
-
-		return falseArr;
-	};
-
-	const [names, setNames] = useState([]);
-
+	// 전공 받아오기
 	useEffect(() => {
-		axios
-			.get('https://jsonplaceholder.typicode.com/posts')
-			.then((r) => setNames(r.data));
+		axios.get(`${process.env.NEXT_PUBLIC_API}/departments`).then((r) => {
+			setDepartmentList(r.data.result);
+		});
 	}, []);
 
+	const { closeModal } = useModal();
+	const [department, setDepartment] = useRecoilState(DepartmentAtom);
+
 	return (
-		<AnimatePresence>
-			{showModal && (
-				<ModalLayout title='정렬' href={href} variant='large'>
-					{/* TODO: 여기 높이 바꿔야함 */}
-					<ul className='flex-col w-full overflow-scroll h-[1000px] scrollbar-hide'>
-						{names.map((item) => (
-							<li
-								key={item.id}
-								// className={`mb-[32px] ${selectedSort[i] && 'text-100'}`}
-							>
-								{item.id}
-							</li>
-						))}
-					</ul>
-				</ModalLayout>
-			)}
-		</AnimatePresence>
+		<ModalLayout
+			title='정렬'
+			onClose={() => closeModal('department')}
+			variant='large'
+			key='department'
+		>
+			{/* TODO: 여기 높이 바꿔야함 */}
+			<ul className='flex-col w-full overflow-scroll h-[412px] scrollbar-hide'>
+				{departmentList.map((name) => (
+					<li
+						key={name}
+						className={`py-[16px]  ${name === department && 'text-100'}`}
+						onClick={() => {
+							setDepartment(name);
+							closeModal('department');
+						}}
+					>
+						{name}
+					</li>
+				))}
+			</ul>
+			<div className='w-full h-[10px] m-[30px]' />
+		</ModalLayout>
 	);
 }
 
