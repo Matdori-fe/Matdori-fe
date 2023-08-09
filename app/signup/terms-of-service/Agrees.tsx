d70fb9c362107f172b42ebe3b1a6b2294140de11
pakxe <pigkill40@naver.com>
2023-08-09 19:36:27 +0900

    [MATDORI-56] feat: 약관 페이지인 terms-of-service 페이지 완성

---
+++ app/signup/terms-of-service/Agrees.tsx
@@ -0,0 +1,117 @@
+'use client';
+
+import { useState } from 'react';
+import Agree from './Agree';
+import Button from '@/components/Button/Button';
+import { RegisterEmailAtom } from '@/app/status/RegisterEmailAtom';
+import { useRecoilValue } from 'recoil';
+import { PasswordAtom } from '@/app/status/PasswordAtom';
+import { DepartmentAtom } from '@/app/status/DepartmentAtom';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import Toast from '@/components/Toast/Toast';
+import { scrollToBottom } from '@/utils/page/moveToBottom';
+
+// TODO: 여기에 회원가입 로직이 존재함. 리펙터링 필요
+const agreeList = [
+	'이용 약관에 동의합니다.',
+	'개인정보 수집 및 이용에 동의합니다.',
+	'만 14세 이상입니다.',
+];
+
+export default function Agrees() {
+	const router = useRouter();
+	const [checkList, setCheckList] = useState<string[]>([]);
+	const email = useRecoilValue(RegisterEmailAtom);
+	const password = useRecoilValue(PasswordAtom);
+	const department = useRecoilValue(DepartmentAtom);
+	const [error, setError] = useState('not');
+
+	const signup = async () => {
+		try {
+			const result = await axios.post(
+				`${process.env.NEXT_PUBLIC_API}/sign-up`,
+				{
+					email,
+					password: password.password,
+					department,
+				},
+				{
+					withCredentials: true,
+				}
+			);
+
+			router.push('/signup/complete');
+		} catch (error) {
+			// console.log(error.response.status);
+			const status = error.response.status;
+
+			// 다시 인증
+			if (status === 401) {
+				Toast('메일 인증을 다시 진행해주세요.');
+			}
+
+			if (status === 409) {
+				Toast('이미 존재하는 회원입니다.');
+			}
+		}
+
+		// const { status } = result.data;
+
+		// if (status === 200) {
+		// 	console.log('회원가입 완료');
+		// 	router.push('/signup/complete');
+		// }
+
+		// if (status === 401) {
+		// 	console.log('회원가입 실패');
+		// 	setError('errer');
+		// 	Toast('메일 인증을 다시 진행해주세요.');
+		// }
+
+		// if (status === 409) {
+		// 	console.log('회원가입 실패');
+		// 	Toast('이미 가입된 회원입니다.');
+		// }
+	};
+
+	return (
+		<>
+			{agreeList.map((item) => (
+				<>
+					<Agree
+						label={item}
+						variant={checkList.includes(item) ? 'check' : 'unCheck'}
+						onClick={() => {
+							if (checkList.includes(item))
+								setCheckList(checkList.filter((i) => i !== item));
+							else setCheckList((prev) => [...prev, item]);
+						}}
+					/>
+				</>
+			))}
+
+			<Button
+				variant='active'
+				label={checkList.length === 3 ? '회원가입 완료하기' : '모두 동의하기'}
+				onClick={
+					checkList.length === 3
+						? () => {
+								signup();
+						  }
+						: () => {
+								console.log(document.body.scrollHeight);
+								scrollToBottom();
+
+								setCheckList([...agreeList]);
+						  }
+				}
+			/>
+		</>
+	);
+}
+
+/**
+ * 다 입력 안했으면 토스트로 에러 메세지 띄워야함
+ * 다 선택했으면 버튼 이름 바꾸고 페이지 이동으로 바꾼다.
+ */
---
+++ app/signup/terms-of-service/page.tsx
@@ -0,0 +1,47 @@
+import HorizonBar from '@/components/HorizonBar/HorizonBar';
+import Text from '@/components/Text/Text';
+import SmallTitle from '@/components/Title/SmallTitle';
+import Agree from './Agree';
+import Agrees from './Agrees';
+
+// TODO: api 호출해서 약관 내용 받아오기
+export default async function Registration() {
+	const data = await getServiceRule();
+
+	return (
+		<div className='w-full [&>*]:mb-[14px]'>
+			{data.map((rule) => (
+				<>
+					<SmallTitle>{rule.title}</SmallTitle>
+					<Text>{rule.contents}</Text>
+					<HorizonBar />
+				</>
+			))}
+			{data.map((rule) => (
+				<>
+					<SmallTitle>{rule.title}</SmallTitle>
+					<Text>{rule.contents}</Text>
+					<HorizonBar />
+				</>
+			))}
+			{data.map((rule) => (
+				<>
+					<SmallTitle>{rule.title}</SmallTitle>
+					<Text>{rule.contents}</Text>
+					<HorizonBar />
+				</>
+			))}
+			<Agrees />
+		</div>
+	);
+}
+
+const getServiceRule = async () => {
+	const result = await fetch(
+		`${process.env.NEXT_PUBLIC_API}/terms-of-service`,
+		{ cache: 'no-store' }
+	);
+
+	const data = await result.json();
+	return data.result;
+};
