9e963a8f38b90967b0108d21597fe21ab2f8829e
pakxe <pigkill40@naver.com>
2023-08-09 19:44:54 +0900

    [MATDORI-56] feat: 모달을 켜고 끄기 위한 훅인 useModal 완료

--- hooks/useModal.tsx
+++ hooks/useModal.tsx
@@ -1,23 +1,45 @@
-import { modalState } from '@/app/status/ModalAtom';
+// import { modalState } from '@/app/status/ModalsAtom';
+// import { useRecoilState } from 'recoil';
+
+import { ModalsAtom } from '@/app/status/ModalsAtom';
+import { FunctionComponent } from 'react';
 import { useRecoilState } from 'recoil';
 
-type OpenModalType = {
-	title: string;
-	content: JSX.Element | string;
-	callback?: () => any;
-};
+// type OpenModalType = {
+// 	title: string;
+// 	content: JSX.Element | string;
+// 	callback?: () => any;
+// };
+
+// export const useModal = () => {
+// 	const [modalDataState, setModalDataState] = useRecoilState(modalState);
+
+// 	const closeModal = () => {
+// 		setModalDataState((prev) => {
+// 			return { ...prev, isOpen: false };
+// 		});
+// 	};
+
+// 	const openModal = ({ title, content, callback }: OpenModalType) =>
+// 		setModalDataState({ isOpen: true, title, content, callBack: callback });
+
+// 	return { modalDataState, closeModal, openModal };
+// };
 
 export const useModal = () => {
-	const [modalDataState, setModalDataState] = useRecoilState(modalState);
+	const [modals, setModals] = useRecoilState(ModalsAtom);
 
-	const closeModal = () => {
-		setModalDataState((prev) => {
-			return { ...prev, isOpen: false };
-		});
+	const openModal = (id: string, Component: FunctionComponent) => {
+		setModals((prev) => [...prev, { id, Component }]);
 	};
 
-	const openModal = ({ title, content, callback }: OpenModalType) =>
-		setModalDataState({ isOpen: true, title, content, callBack: callback });
+	const closeModal = (id: string) => {
+		setModals((prev) => prev.filter((component) => component.id !== id));
+	};
 
-	return { modalDataState, closeModal, openModal };
+	return {
+		modals,
+		openModal,
+		closeModal,
+	};
 };
