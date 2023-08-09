f478cd492e8b2dfcee3e6b43b86a0f7e683ba15c
pakxe <pigkill40@naver.com>
2023-08-09 20:44:16 +0900

    [MATDORI-56] fix: 토스트의 글씨가 줄넘김 되는 것을 수정

--- components/Toast/Toast.tsx
+++ components/Toast/Toast.tsx
@@ -7,7 +7,7 @@ export function Toast(message: string) {
 	toast(message, {
 		clickClosable: true,
 		render: (message) => (
-			<div className='bg-neutral-400 h-[47px] sm:w-[calc(768px-40px)] mb-[58px] px-[10px] flex items-center justify-center rounded-basic'>
+			<div className='bg-neutral-200 h-[47px] sm:w-[calc(412px-40px)] w-[calc(100vw-40px)] mb-[58px] px-[10px] flex items-center justify-center rounded-basic'>
 				<Text color='white' size='base' fontWeight='semibold'>
 					{message}
 				</Text>
