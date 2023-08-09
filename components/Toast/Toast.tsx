3b7ab5cb62c2be3c9acab6621b9ee6e43c3131dc
pakxe <pigkill40@naver.com>
2023-08-09 19:44:23 +0900

    [MATDORI-56] chore: Toast에 고쳐야할 부분 작성

--- components/Toast/Toast.tsx
+++ components/Toast/Toast.tsx
@@ -1,11 +1,13 @@
 import toast from 'react-simple-toasts';
 import Text from '../Text/Text';
 
+// FIXME: 토스트간 여백이 너무 큼
+// FIXME: 토스트 px가 너무 작음
 export function Toast(message: string) {
 	toast(message, {
 		clickClosable: true,
 		render: (message) => (
-			<div className='bg-neutral-400 h-[47px] md:w-[calc(768px-40px)] my-[58px] px-[20px] flex items-center justify-center rounded-basic'>
+			<div className='bg-neutral-400 h-[47px] sm:w-[calc(768px-40px)] mb-[58px] px-[10px] flex items-center justify-center rounded-basic'>
 				<Text color='white' size='base' fontWeight='semibold'>
 					{message}
 				</Text>
