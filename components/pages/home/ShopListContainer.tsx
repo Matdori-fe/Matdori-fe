4fe67e6710dc311ae1a2f812b2a2b6cb63ef8683
pakxe <pigkill40@naver.com>
2023-08-14 21:20:29 +0900

    [MATDORI-58] feat: 무한 스크롤 아이템을 나열하는 ShopListContainer 완성

---
+++ components/pages/home/ShopListContainer.tsx
@@ -0,0 +1,14 @@
+import ShopItem from './ShopItem';
+
+export default function ShopListContainer({ list }) {
+	return (
+		<>
+			<div className='flex flex-col gap-[15px]'>
+				{list.map(() => (
+					<ShopItem />
+				))}
+			</div>
+			<div className='mb-[80px]'></div>
+		</>
+	);
+}
