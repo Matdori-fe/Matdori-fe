3a62ece75ced4fe8b0e75a0ed86e7700d7dc65d5
pakxe <pigkill40@naver.com>
2023-08-15 00:02:54 +0900

    [MATDORI-58] feat: 홈 화면의 캐러셀인 CarouselSection 완성

---
+++ components/pages/home/CarouselSection.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const CarouselSection = () => {
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true, // 자동 캐러셀
+		autoplaySpeed: 4000,
+		draggable: true,
+		arrows: false,
+	};
+
+	const arr = [1, 2, 3, 4, 5];
+
+	return (
+		<div className='absolute left-0 top-[60px] w-full [&+*]:mt-[330px]'>
+			<Slider {...settings} dotsClass='test-css'>
+				{arr.map((i) => (
+					<div className='w-full h-[211px] bg-10'>{i}</div>
+				))}
+			</Slider>
+		</div>
+	);
+};
+export default CarouselSection;
