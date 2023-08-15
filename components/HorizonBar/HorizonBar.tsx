c47f2ee9a485cc36ccbfb7b290dba980c1cd57dd
pakxe <pigkill40@naver.com>
2023-08-15 23:07:12 +0900

    [MATDORI-58] fix: 수평선이 전체 width를 차지하도록 수정

--- components/HorizonBar/HorizonBar.tsx
+++ components/HorizonBar/HorizonBar.tsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React from 'react';
 
 // 사용방법: 테일윈드에 두께랑 여백을 기호에 맞게 조절하세요.
 // 예시: <HorizonBar className="border-t-[3px] mx-[10px]"/>
 
 type HorizonType = {
-  className?: string;
+	className?: string;
 };
 
 const HorizonBar: React.FC<HorizonType> = ({
-  className = "border-t-[1px] mx-[0]",
+	className = 'border-t-[1px] mx-[0]',
 }) => {
-  return (
-    <>
-      <div
-        className={`w-full border-t-[1px] border-lightGray ${className}`}
-      ></div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className={`absolute left-0 w-full border-t-[1px] border-lightGray bg-lightGray [&+*]:mt-[7px] ${className} z-1`}
+			></div>
+		</>
+	);
 };
 
 export default HorizonBar;
