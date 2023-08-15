496fd56daa78a27b5310d5c1d12f6c8f8114b451
pakxe <pigkill40@naver.com>
2023-08-16 00:50:33 +0900

    [MATDORI-62] style: 로딩 컴포넌트의 높이 늘림

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -3,7 +3,7 @@ import loading from '../../public/loading.gif';
 
 export default function Loading() {
 	return (
-		<div className='flex justify-center w-full'>
+		<div className='flex justify-center w-full h-[50px] items-center'>
 			<Image src={loading} alt='loading' height='50' width='50' />
 		</div>
 	);
