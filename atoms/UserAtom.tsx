950f421d7a8e7d8e979cce38d85b5d3c0206a039
pakxe <pigkill40@naver.com>
2023-08-15 23:01:34 +0900

    [MATDORI-58] feat: 유저 데이터를 저장하기 위한 상태인 UserAtom 완성

---
+++ atoms/UserAtom.tsx
@@ -0,0 +1,16 @@
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+type UserInfo = {
+	department: string;
+	nickname: string;
+	userId: number;
+};
+
+const { persistAtom } = recoilPersist();
+
+export const UserAtom = atom<UserInfo>({
+	key: 'user',
+	default: { department: 'none', nickname: 'none', userId: 0 },
+	effects_UNSTABLE: [persistAtom],
+});
