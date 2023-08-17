98d6bd0c0a3a651c9e9e15210bb55bc102406f65
pakxe <pigkill40@naver.com>
2023-08-17 23:45:01 +0900

    [MATDORI-62] feat: 항목을 지우기 위한 DeleteButton 완성

---
+++ components/DeleteButton/DeleteButton.tsx
@@ -0,0 +1,9 @@
+import { RiCloseFill } from 'react-icons/ri';
+
+export default function DeleteButton() {
+	return (
+		<div className='h-[20px] w-[20px] rounded-basic bg-white border-lightGray border flex justify-center items-center'>
+			<RiCloseFill size='10' />
+		</div>
+	);
+}
