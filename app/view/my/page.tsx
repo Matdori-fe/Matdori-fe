405f4db6ba638ea91d5eea9513534079715d0194
pakxe <pigkill40@naver.com>
2023-08-17 16:24:22 +0900

    [MATDORI-62] feat: my-page 페이지 완성

--- app/view/my/page.tsx
+++ app/view/my/page.tsx
@@ -1,5 +1,14 @@
+import LikesAndActivitySection from '@/components/pages/my-page/LikesAndActivitySection';
+import OptionSection from '@/components/pages/my-page/OptionSection';
+
 const My = () => {
-  return <>My</>;
+	return (
+		<div>
+			<div className='mt-[80px] w-full h-[1px]' />
+			<LikesAndActivitySection />
+			<OptionSection />
+		</div>
+	);
 };
 
 export default My;
