3f28fa5da43dbd7e67cd24cb45e2d1208bb93488
pakxe <pigkill40@naver.com>
2023-08-17 14:16:26 +0900

    [MATDORI-62] feat: href를 받으면 해당 링크로 이동

--- components/RoundButton/RoundButton.tsx
+++ components/RoundButton/RoundButton.tsx
@@ -1,14 +1,22 @@
+import { Link } from 'react-router-dom';
 import Text from '../Text/Text';
+import { useRouter } from 'next/navigation';
 
 interface ButtonProps {
 	label: string;
 	onClick?: () => void;
+	href?: string;
 }
 
-export default function RoundButton({ label, onClick }: ButtonProps) {
+export default function RoundButton({
+	label,
+	onClick,
+	href = '',
+}: ButtonProps) {
+	const router = useRouter();
 	return (
 		<button
-			onClick={onClick}
+			onClick={href ? () => router.push(href) : onClick}
 			className='px-[10px] py-[5px] bg-white rounded-2xl border border-lightGray justify-center items-center inline-flex'
 		>
 			<Text color='darkGray' size='xxs' fontWeight='normal'>
