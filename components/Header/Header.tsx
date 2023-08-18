e6410420effb7170f1fdbf0650ac28b5e5d3f487
pakxe <pigkill40@naver.com>
2023-08-18 23:25:04 +0900

    [MATDORI-71] feat: z-index 추가

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,57 +4,57 @@ import HeaderRight from './HeaderRight';
 
 export type Left = 'back' | 'logo' | undefined;
 export type Right =
-  | 'search'
-  | 'share'
-  | 'like'
-  | 'more'
-  | 'trashCan'
-  | 'check'
-  | 'roundButton'
-  | undefined;
+	| 'search'
+	| 'share'
+	| 'like'
+	| 'more'
+	| 'trashCan'
+	| 'check'
+	| 'roundButton'
+	| undefined;
 
 type LikeKind = 'store' | 'jokbo' | 'review';
 
 interface HeaderProps {
-  left?: Left;
-  right?: Right[] | Right;
-  title?: string;
-  kind?: LikeKind; // 추가: kind 값
-  id?: number;
-  inFavoriteId?: number | null;
+	left?: Left;
+	right?: Right[] | Right;
+	title?: string;
+	kind?: LikeKind; // 추가: kind 값
+	id?: number;
+	inFavoriteId?: number | null;
 }
 
 export default function Header({
-  left,
-  right,
-  title,
-  kind,
-  id,
-  inFavoriteId,
+	left,
+	right,
+	title,
+	kind,
+	id,
+	inFavoriteId,
 }: HeaderProps) {
-  return (
-    <div className="flex justify-center [&+*]:mt-[60px] bg-white">
-      <div className="z-2 sm:w-[412px] w-full h-[60px] flex items-center px-[20px] gap-3.5 [&_p]:flex-1 fixed top-0 bg-white">
-        <HeaderLeft left={left} />
-        <Text size="lg" fontWeight="bold">
-          {title}
-        </Text>
-        {/*좋아요라면, kind, id, inFavoriteId를 넘겨줘야함. */}
-        {right && kind && id && inFavoriteId ? (
-          <>
-            <HeaderRight
-              right={right}
-              kind={kind}
-              id={id}
-              inFavoriteId={inFavoriteId}
-            />
-          </>
-        ) : (
-          <>
-            <HeaderRight right={right} />
-          </>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex justify-center [&+*]:mt-[60px] bg-white'>
+			<div className='z-2 sm:w-[412px] w-full h-[60px] flex items-center px-[20px] gap-3.5 [&_p]:flex-1 fixed top-0 bg-white'>
+				<HeaderLeft left={left} />
+				<Text size='lg' fontWeight='bold'>
+					{title}
+				</Text>
+				{/*좋아요라면, kind, id, inFavoriteId를 넘겨줘야함. */}
+				{right && kind && id && inFavoriteId ? (
+					<>
+						<HeaderRight
+							right={right}
+							kind={kind}
+							id={id}
+							inFavoriteId={inFavoriteId}
+						/>
+					</>
+				) : (
+					<>
+						<HeaderRight right={right} />
+					</>
+				)}
+			</div>
+		</div>
+	);
 }
