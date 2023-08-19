a7536b2aacbad625c68611e080eeb65ee70067f7
pakxe <pigkill40@naver.com>
2023-08-20 01:25:00 +0900

    [MATDORI-71] feat: delete 상태일 경우 체크 아이콘으로 변경

--- components/Header/TrashCan.tsx
+++ components/Header/TrashCan.tsx
@@ -2,16 +2,27 @@
 
 import { deleteAtom } from '@/atoms/deleteAtom';
 import { FiTrash2 } from 'react-icons/fi';
+import { RiCheckFill } from 'react-icons/ri';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 
 export default function TrashCan() {
 	const [deleteState, setDeleteAtom] = useRecoilState(deleteAtom);
 
 	return (
-		<FiTrash2
-			size='20'
-			className='text-100'
-			onClick={() => setDeleteAtom(!deleteState)}
-		/>
+		<>
+			{deleteState ? (
+				<RiCheckFill
+					size='20'
+					className='fill-100'
+					onClick={() => setDeleteAtom(!deleteState)}
+				/>
+			) : (
+				<FiTrash2
+					size='20'
+					className='text-100'
+					onClick={() => setDeleteAtom(!deleteState)}
+				/>
+			)}
+		</>
 	);
 }
