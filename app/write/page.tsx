41d7fe6a91d62b37bb74282bbc82a6ee0ff4d011
sean <sean2337@naver.com>
2023-08-21 22:59:41 +0900

    [MATDORI-74] feat: Qna 페이지 완성

---
+++ app/write/page.tsx
@@ -0,0 +1,5 @@
+const WritePage = () => {
+  return <>write Page</>;
+};
+
+export default WritePage;
--- components/Text/Text.tsx
+++ components/Text/Text.tsx
@@ -1,60 +1,60 @@
 interface TextProps {
-	size?: 'xxs' | 'xs' | 'sm' | 'base' | 'lg'; // 9, 12, 14, 16, 18
-	color?: string;
-	fontWeight?: 'normal' | 'medium' | 'semibold' | 'bold';
-	children: React.ReactNode;
-	className?: string;
+  size?: 'xxs' | 'xs' | 'sm' | 'base' | 'lg'; // 9, 12, 14, 16, 18
+  color?: string;
+  fontWeight?: 'normal' | 'medium' | 'semibold' | 'bold';
+  children: React.ReactNode;
+  className?: string;
 }
 
 type Type = {
-	[key: string]: string;
+  [key: string]: string;
 };
 
 type ColorType = {
-	[key: number]: string;
-	[key: string]: string;
+  [key: number]: string;
+  [key: string]: string;
 };
 
 const Size: Type = {
-	xxs: 'text-xxs',
-	xs: 'text-xs',
-	sm: 'text-sm',
-	base: 'text-base',
-	lg: 'text-lg',
+  xxs: 'text-xxs',
+  xs: 'text-xs',
+  sm: 'text-sm',
+  base: 'text-base',
+  lg: 'text-lg',
 };
 
 const Color: ColorType = {
-	100: 'text-100',
-	80: 'text-80',
-	30: 'text-30',
-	10: 'text-10',
-	black: 'text-black',
-	white: 'text-white',
-	gray: 'text-gray',
-	lightGray: 'text-lightGray',
-	darkGray: 'text-darkGray',
-	blue: 'text-blue',
+  100: 'text-100',
+  80: 'text-80',
+  30: 'text-30',
+  10: 'text-10',
+  black: 'text-black',
+  white: 'text-white',
+  gray: 'text-gray',
+  lightGray: 'text-lightGray',
+  darkGray: 'text-darkGray',
+  blue: 'text-blue',
 };
 
 const FontWeight: Type = {
-	normal: 'font-normal',
-	medium: 'font-medium',
-	semibold: 'font-semibold',
-	bold: 'font-bold',
+  normal: 'font-normal',
+  medium: 'font-medium',
+  semibold: 'font-semibold',
+  bold: 'font-bold',
 };
 
 export default function Text({
-	size = 'base',
-	color = 'black',
-	fontWeight = 'normal',
-	children,
-	className,
+  size = 'base',
+  color = 'black',
+  fontWeight = 'normal',
+  children,
+  className,
 }: TextProps) {
-	return (
-		<p
-			className={`mt-[1px] ${Size[size]} ${Color[color]} ${FontWeight[fontWeight]} ${className}`}
-		>
-			{children}
-		</p>
-	);
+  return (
+    <p
+      className={`mt-[1px] ${Size[size]} ${Color[color]} ${FontWeight[fontWeight]} ${className}`}
+    >
+      {children}
+    </p>
+  );
 }
