659e3cc81a151a6e7509033bae4b3d6659994156
pakxe <pigkill40@naver.com>
2023-08-23 02:50:57 +0900

    [MATDORI-71] feat: 배열 요소가 있는 set의 has 메서드 구현

---
+++ lib/setHasArr.ts
@@ -0,0 +1,14 @@
+interface ISetHasArr {
+	set: Set<number[]>;
+	key: number[];
+}
+
+export default function setHasArr({ set, key }: ISetHasArr) {
+	let haveItem = false;
+
+	set.forEach((item) => {
+		if (JSON.stringify(item) === JSON.stringify(key)) haveItem = true;
+	});
+
+	return haveItem;
+}
