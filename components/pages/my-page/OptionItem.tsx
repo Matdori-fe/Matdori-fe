067e4922aca44ed6ff457ac41045e21e39ebf3f6
pakxe <pigkill40@naver.com>
2023-08-23 00:59:40 +0900

    [MATDORI-71] feat: 마이페이지에서 아이템을 보여주기 위한 OptionItem 완성

---
+++ components/pages/my-page/OptionItem.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Text from '@/components/Text/Text';
+import { IOption } from './OptionSection';
+import { RiBilibiliFill } from 'react-icons/ri';
+import HorizonBar from '@/components/HorizonBar/HorizonBar';
+import router from 'next/router';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+export default function OptionItem(item: IOption) {
+	const router = useRouter();
+
+	return (
+		<div onClick={item.href ? () => router.push(item.href) : item.onClick}>
+			<div className='h-[48px] flex items-center gap-[10px] px-[10px]'>
+				<div className='text-sm text-100'>{item.icon}</div>
+				<Text size='sm' fontWeight='semibold'>
+					{item.name}
+				</Text>
+			</div>
+			<div className='h-[1px] bg-lightGray w-full' />
+		</div>
+	);
+}
