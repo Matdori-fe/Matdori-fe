63b4808f944938a8c7891621d12363377a10a62b
pakxe <pigkill40@naver.com>
2023-08-24 23:07:46 +0900

    [MATDORI-71] feat: 삭제 모드면 x버튼을 띄우도록 수정

--- components/Header/HeaderLeft.tsx
+++ components/Header/HeaderLeft.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import logo from '../../assets/image/logo.svg';
 import { moveToTop } from '@/utils/page/moveToTop';
 import { moveToBack } from '@/utils/page/moveToBack';
+import DeleteModeCancel from './DeleteModeCancel';
 
 const leftIcons = {
 	back: <RiArrowLeftLine onClick={moveToBack} size='24' />,
@@ -13,7 +14,7 @@ const leftIcons = {
 };
 
 export default function HeaderLeft({ left }: { left: Left }) {
-	return <>{left && <button>{left && leftIcons[left]}</button>}</>;
+	return <>{left === 'logo' ? leftIcons[left] : <DeleteModeCancel />}</>;
 }
 
 // FIXME: back일 때 클릭 영역이 작음. 이것이 문제가 될 지는 실제 손꾸락으로 봐야 알 것 같아서 .. 나중에 고쳐봄
