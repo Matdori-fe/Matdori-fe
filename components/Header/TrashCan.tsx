bdb702e8316b28140eb11d4686092142e989d696
pakxe <pigkill40@naver.com>
2023-08-24 22:05:51 +0900

    [MATDORI-71] feat: 삭제 모드인 경우 아무 아이콘도 띄우지 않도록 변경

--- components/Header/TrashCan.tsx
+++ components/Header/TrashCan.tsx
@@ -1,26 +1,20 @@
 'use client';
 
-import { deleteAtom } from '@/atoms/deleteAtom';
+import { deleteAtom } from '@/atoms/delete';
 import { FiTrash2 } from 'react-icons/fi';
 import { RiCheckFill } from 'react-icons/ri';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 
 export default function TrashCan() {
-	const [deleteState, setDeleteAtom] = useRecoilState(deleteAtom);
+	const [deleteMode, setDeleteMode] = useRecoilState(deleteAtom);
 
 	return (
 		<>
-			{deleteState ? (
-				<RiCheckFill
-					size='20'
-					className='fill-100'
-					onClick={() => setDeleteAtom(!deleteState)}
-				/>
-			) : (
+			{!deleteMode && (
 				<FiTrash2
 					size='20'
 					className='text-100'
-					onClick={() => setDeleteAtom(!deleteState)}
+					onClick={() => setDeleteMode(true)}
 				/>
 			)}
 		</>
