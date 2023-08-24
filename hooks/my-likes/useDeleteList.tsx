fd90aadd6faeed67fe4aa173ca3b8965664f8ab7
pakxe <pigkill40@naver.com>
2023-08-24 21:59:45 +0900

    [MATDORI-71] feat: 선택 리스트를 조작하기 위한 핸들러 작성

--- hooks/my-likes/useDeleteList.tsx
+++ hooks/my-likes/useDeleteList.tsx
@@ -1,18 +1,24 @@
-import { deleteListAtom } from '@/atoms/delete';
-import { useRecoilState } from 'recoil';
+import { checkedListAtom, deleteAtom, deleteListAtom } from '@/atoms/delete';
+import setHasArr from '@/lib/setHasArr';
+import { useEffect } from 'react';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
 export const useDeleteList = () => {
 	const [deleteList, setDeleteList] = useRecoilState(deleteListAtom);
+	const [checkedList, setCheckedList] = useRecoilState(checkedListAtom);
+	const deleteMode = useRecoilValue(deleteAtom);
 
-	const pushItem = (firstId: number, secondId: number | undefined) => {
+	const pushItem = (firstId: number, secondId?: number | undefined) => {
 		// 하나의 id만 있는 경우
 		if (secondId === undefined)
 			setDeleteList(() => new Set([...deleteList, firstId]));
 		// 두개의 id가 있는 경우
 		else setDeleteList(() => new Set([...deleteList, [firstId, secondId]]));
+
+		setCheckedList([...checkedList, firstId]);
 	};
 
-	const deleteItem = (firstId: number, secondId: number | undefined) => {
+	const deleteItem = (firstId: number, secondId?: number | undefined) => {
 		const prev = new Set([...deleteList]); // 리스트 복사
 
 		// 하나의 id만 있는 경우
@@ -38,7 +44,33 @@ export const useDeleteList = () => {
 
 			setDeleteList(new Set([...prev]));
 		}
+
+		// TODO: 여기에 체크리스트에서 제거하는 로직 필요(배열 delete)
 	};
 
-	return { pushItem, deleteItem, deleteList };
+	const handleItem = (firstId: number, secondId: number | undefined) => {
+		if (secondId === undefined) {
+			if (deleteList.has(firstId)) deleteItem(firstId);
+			else pushItem(firstId);
+		} else {
+			if (
+				setHasArr({
+					set: deleteList as Set<number[]>,
+					key: [firstId, secondId],
+				})
+			)
+				deleteItem(firstId, secondId);
+			else pushItem(firstId, secondId);
+		}
+	};
+
+	const resetItems = () => {
+		setDeleteList(new Set());
+	};
+
+	useEffect(() => {
+		if (!deleteMode && deleteList.size !== 0) resetItems();
+	}, [deleteMode]);
+
+	return { handleItem, resetItems };
 };
