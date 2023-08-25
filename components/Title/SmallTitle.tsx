6b008d349541517aec8e9bb1764add331e2066a4
pakxe <pigkill40@naver.com>
2023-08-25 20:17:56 +0900

    [MATDORI-73] style: 불필요한 패딩 제거

--- components/Title/SmallTitle.tsx
+++ components/Title/SmallTitle.tsx
@@ -1,27 +1,27 @@
 // 사용법 => <SmallTitle sideComponent={들어갈 컴포넌트}>제목</SmallTitle>
 
 type childrenProps = {
-  sideComponent?: React.ReactElement;
-  children: React.ReactNode;
-  className?: string;
+	sideComponent?: React.ReactElement;
+	children: React.ReactNode;
+	className?: string;
 };
 
 // "w-[372px] sm:w-[calc(100%)] h-[40px] bg-lightGray flex justify-between items-center rounded-xl px-4";
 const SmallTitle: React.FunctionComponent<childrenProps> = ({
-  sideComponent,
-  children,
-  className,
+	sideComponent,
+	children,
+	className,
 }) => {
-  return (
-    <div className="flex justify-center w-full bg-white">
-      <div
-        className={`w-[412px] sm:w-[calc(100%)] flex justify-between items-center px-4 ${className}`}
-      >
-        <p className={`font-SemiBold text-[14px] line-clamp-1`}>{children}</p>
-        {sideComponent}
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex justify-center w-full bg-white'>
+			<div
+				className={`w-[412px] sm:w-[calc(100%)] flex justify-between items-center ${className}`}
+			>
+				<p className={`font-SemiBold text-[14px] line-clamp-1`}>{children}</p>
+				{sideComponent}
+			</div>
+		</div>
+	);
 };
 
 export default SmallTitle;
