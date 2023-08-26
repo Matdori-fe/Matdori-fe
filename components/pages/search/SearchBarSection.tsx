1a13463eb1b3cd9ba154e2529b2218d51d679213
pakxe <pigkill40@naver.com>
2023-08-27 00:41:22 +0900

    [MATDORI-64] feat: 검색어를 비우는 x버튼을 누르면 검색 초기 화면으로 이동하도록 수정

--- components/pages/search/SearchBarSection.tsx
+++ components/pages/search/SearchBarSection.tsx
@@ -1,6 +1,15 @@
+'use client';
+
+import { searchAtom } from '@/atoms/search/searchAtom';
 import Input from '@/components/Input/Input';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
 
 export default function SearchBarSection() {
+	const [searchInput, setSearchInput] = useRecoilState(searchAtom);
+	const router = useRouter();
+
 	return (
 		<div>
 			<Input
@@ -8,6 +17,11 @@ export default function SearchBarSection() {
 				left='back'
 				placeHolder='무엇이든 검색해보세요.'
 				right='cancel'
+				value={searchInput}
+				onChange={(e) => {
+					if (e.target.value === '') router.push('/search');
+					setSearchInput(e.target.value);
+				}}
 			/>
 		</div>
 	);
