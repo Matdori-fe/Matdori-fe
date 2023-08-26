800215190449df141633486909bed843f5ce70ba
pakxe <pigkill40@naver.com>
2023-08-27 00:44:58 +0900

    [MATDORI-64] feat: 수평바 다른 컴포넌트로 수정

--- components/pages/home/InfiniteShopListSection.tsx
+++ components/pages/home/InfiniteShopListSection.tsx
@@ -1,13 +1,15 @@
 import BigTitle from '@/components/Title/BigTitle';
 import ShopItem from './ShopItem';
 import ShopListContainer from './ShopListContainer';
+import HorizonBar from '@/components/HorizonBar/HorizonBar';
 
 export default function InfiniteShopListSection() {
 	const a = [1, 2, 3, 4, 5];
 	return (
-		<>
-			<BigTitle className='mb-[15px]'>골라먹는 맛집</BigTitle>
+		<div className='flex flex-col gap-[16px]'>
+			<div className='bg-lightGray h-[7px] w-[100vw] relative left-[-20px]' />
+			<BigTitle>골라먹는 맛집</BigTitle>
 			<ShopListContainer list={a} />
-		</>
+		</div>
 	);
 }
