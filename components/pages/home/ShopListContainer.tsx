447d56eb5da8c506186cd96e63cea28664d5f39b
pakxe <pigkill40@naver.com>
2023-08-27 00:43:44 +0900

    [MATDORI-64] chore: div로 감싸기

--- components/pages/home/ShopListContainer.tsx
+++ components/pages/home/ShopListContainer.tsx
@@ -2,13 +2,13 @@ import ShopItem from './ShopItem';
 
 export default function ShopListContainer({ list }) {
 	return (
-		<>
+		<div>
 			<div className='flex flex-col gap-[15px]'>
 				{list.map(() => (
 					<ShopItem />
 				))}
 			</div>
 			<div className='mb-[80px]'></div>
-		</>
+		</div>
 	);
 }
