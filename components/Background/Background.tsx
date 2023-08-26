4da15c42f2eca3b4a91f3b5cb62baa565065cfe5
pakxe <pigkill40@naver.com>
2023-08-27 00:49:21 +0900

    [MATDORI-64] style: z-index 더 높게 수정

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -10,7 +10,7 @@ export default function Background({
 	return (
 		<div
 			onClick={onClose}
-			className={`z-3 fixed w-screen h-full bg-transparent z-1 top-0 left-0 ${className} flex items-end`}
+			className={`z-20 fixed w-screen h-full bg-transparent z-1 top-0 left-0 ${className} flex items-end`}
 		>
 			{children}
 		</div>
