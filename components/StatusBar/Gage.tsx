cd8fc95694c27ad987b4503b3857dd2570d149c2
pakxe <pigkill40@naver.com>
2023-08-28 17:13:48 +0900

    [MATDORI-106] fix: 소수점이 표시되지 않던 문제 해결

--- components/StatusBar/Gage.tsx
+++ components/StatusBar/Gage.tsx
@@ -6,7 +6,7 @@ const StyledGage = styled.div<{ score: number }>`
 	width: ${({ score }) => score * 12}px;
 `;
 
-export default function Gage({ score }: { score: number }) {
+export default function Gage({ score }: { score: number | string }) {
 	return (
 		<StyledGage
 			score={score}
