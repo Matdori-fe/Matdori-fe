72d1ae75ec4312ec6c6f30cc058e5dc858e5ec19
pakxe <pigkill40@naver.com>
2023-08-29 21:45:24 +0900

    [MATDORI-101] feat: 버전 정보 페이지 완성

---
+++ app/version/page.tsx
@@ -0,0 +1,7 @@
+import HorizonBar from '@/components/HorizonBar/HorizonBar';
+import Text from '@/components/Text/Text';
+import SmallTitle from '@/components/Title/SmallTitle';
+
+export default function TermsOfPage() {
+	return <div className='w-full'>1.0.0</div>;
+}
