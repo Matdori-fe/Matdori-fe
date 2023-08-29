8ec7e99d8719fc737ea9e6a2e3e18ef86336b3a0
pakxe <pigkill40@naver.com>
2023-08-29 11:10:31 +0900

    [MATDORI-106] feat: Top3Section 작성 완료

--- components/pages/home/Top3Section.tsx
+++ components/pages/home/Top3Section.tsx
@@ -1,3 +1,35 @@
+'use client';
+
+import SmallTitle from '@/components/Title/SmallTitle';
+import BigTitle from '@/components/Title/BigTitle';
+import Top3Item from './Top3Item';
+
+import SortWrapper from './SortWrapper';
+import { useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { Top3List, top3SortAtom } from '@/atoms/home/top3SortAtom';
+import { useRecoilValue } from 'recoil';
+import { getTop3List } from '@/lib/home/getTop3List';
+import { AxiosResponse } from 'axios';
+
 export default function Top3Section() {
-	return <div>Top3Section</div>;
+	const sortType = useRecoilValue(top3SortAtom);
+	const { data, error, isLoading } = useQuery<AxiosResponse<Top3List>>(
+		'top',
+		() => getTop3List(sortType)
+	);
+
+	return (
+		<div>
+			<div className='flex items-center justify-between mb-[12px]'>
+				<BigTitle>맛도리 TOP 3</BigTitle>
+				<SortWrapper />
+			</div>
+			<div>
+				{data?.data.result.map((item, i) => (
+					<Top3Item {...item} id={i} />
+				))}
+			</div>
+		</div>
+	);
 }
