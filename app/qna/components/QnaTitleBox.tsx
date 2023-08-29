51abd43eb53ea50820c9b3ab281665875059a3d7
pakxe <pigkill40@naver.com>
2023-08-29 11:13:09 +0900

    [MATDORI-106] chore: 따옴표 변경

--- app/qna/components/QnaTitleBox.tsx
+++ app/qna/components/QnaTitleBox.tsx
@@ -4,21 +4,21 @@ import { QnaType } from '../Qna_Type/QnaType';
 import Text from '@/components/Text/Text';
 import Link from 'next/link';
 const QnaTitleBox = ({ qnaIndex, title, content }: QnaType) => {
-  return (
-    <div className="flex justify-between mx-2 py-4 border-b-[1px] border-lightGray">
-      <div className="w-10/12 flex ml-1">
-        <RiQuestionLine className="text-80 text-[16px] mt-[3px] mr-2" />
-        <div className="w-full">
-          <Text fontWeight="semibold" color="black" size="sm">
-            {title}
-          </Text>
-        </div>
-      </div>
-      <Link href={`/qna/${qnaIndex}`}>
-        <RiArrowRightLine className="text-black text-[16px] mt-[3px] mr-1" />
-      </Link>
-    </div>
-  );
+	return (
+		<div className='flex justify-between mx-2 py-4 border-b-[1px] border-lightGray'>
+			<div className='flex w-10/12 ml-1'>
+				<RiQuestionLine className='text-80 text-[16px] mt-[3px] mr-2' />
+				<div className='w-full'>
+					<Text fontWeight='semibold' color='black' size='sm'>
+						{title}
+					</Text>
+				</div>
+			</div>
+			<Link href={`/qna/${qnaIndex}`}>
+				<RiArrowRightLine className='text-black text-[16px] mt-[3px] mr-1' />
+			</Link>
+		</div>
+	);
 };
 
 export default QnaTitleBox;
