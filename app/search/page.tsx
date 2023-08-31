b3c1943cc0196a44a611ffddef52657497963aa9
sean <sean2337@naver.com>
2023-08-31 17:03:35 +0900

    [MATDORI-68] feat: 족보 쓰기 시 검색 부분 기능 추가

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -7,23 +7,27 @@ import RecommendButtonsSection from '@/components/pages/search/RecommendButtonSe
 import RelatedSearchSection from '@/components/pages/search/RelatedSearchSection';
 import SearchBarSection from '@/components/pages/search/SearchBarSection';
 import { useRecoilValue } from 'recoil';
+import { useSearchParams } from 'next/navigation';
 
 const Search = () => {
-	const searchMode = useRecoilValue(searchAtom) === '' ? false : true;
+  const searchMode = useRecoilValue(searchAtom) === '' ? false : true;
+  const searchParams = useSearchParams();
+  const writeStore: boolean = searchParams.get('writeStore') === 'true';
 
-	return (
-		<div className=' flex flex-col gap-[30px]'>
-			{!searchMode ? (
-				<>
-					<RecentSearchListSection />
-					<RecommendButtonsSection />
-					<CategorySection />
-				</>
-			) : (
-				<RelatedSearchSection />
-			)}
-		</div>
-	);
+  return (
+    <div className=" flex flex-col gap-[30px]">
+      {!searchMode ? (
+        <>
+          <RecentSearchListSection />
+          {!writeStore && <RecommendButtonsSection />}
+
+          <CategorySection />
+        </>
+      ) : (
+        <RelatedSearchSection />
+      )}
+    </div>
+  );
 };
 
 export default Search;
--- app/write/components/WriteJokbo.tsx
+++ app/write/components/WriteJokbo.tsx
@@ -4,46 +4,45 @@ import SmallTitle from '@/components/Title/SmallTitle';
 import { useState } from 'react';
 
 const WriteJokbo: React.FC<WriteJokboProps> = ({ setTitle, setContent }) => {
-	const [titleValue, setTitleValue] = useState('');
-	const [contentValue, setContentValue] = useState('');
+  const [titleValue, setTitleValue] = useState('');
+  const [contentValue, setContentValue] = useState('');
 
-	function titleInputHeight(event: React.ChangeEvent<HTMLTextAreaElement>) {
-		const element = event.target;
-		element.style.height = '36px';
-		element.style.height = Math.max(36, element.scrollHeight + 2) + 'px';
-		setTitleValue(element.value);
-		setTitle(element.value);
-	}
+  function titleInputHeight(event: React.ChangeEvent<HTMLInputElement>) {
+    const element = event.target;
 
-	function contentInputHeight(event: React.ChangeEvent<HTMLTextAreaElement>) {
-		const element = event.target;
-		element.style.height = '150px';
-		element.style.height = Math.max(150, element.scrollHeight + 2) + 'px';
-		setContentValue(element.value);
-		setContent(element.value);
-	}
+    setTitleValue(element.value);
+    setTitle(element.value);
+  }
 
-	return (
-		<>
-			<SmallTitle className='w-full'>족보 제목</SmallTitle>
+  function contentInputHeight(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    const element = event.target;
+    element.style.height = '150px';
+    element.style.height = Math.max(150, element.scrollHeight + 2) + 'px';
+    setContentValue(element.value);
+    setContent(element.value);
+  }
 
-			<textarea
-				className='resize-none w-full border-[1px] border-lightGray rounded-basic pt-[8px] pl-3 pr-3 pb-2 text-[12px] font-Regular h-[36px] scrollbar-hide mt-3 mb-3'
-				placeholder='제목을 작성해주세요.'
-				value={titleValue}
-				onChange={titleInputHeight}
-			/>
+  return (
+    <>
+      <SmallTitle className="w-full">족보 제목</SmallTitle>
 
-			<SmallTitle className='w-full'>족보 내용</SmallTitle>
+      <input
+        className="w-full border-[1px] border-lightGray rounded-basic pt-[8px] pl-3 pr-3 pb-2 text-[12px] font-Regular h-[36px] scrollbar-hide mt-3 mb-3"
+        placeholder="제목을 작성해주세요."
+        value={titleValue}
+        onChange={titleInputHeight}
+      />
 
-			<textarea
-				className='w-full border-[1px] border-lightGray rounded-basic p-[5.5px] pt-2 pl-3 pr-3 text-[12px] font-Regular h-[150px] mt-3'
-				placeholder='내용을 작성해주세요.'
-				value={contentValue}
-				onChange={contentInputHeight}
-			/>
-		</>
-	);
+      <SmallTitle className="w-full">족보 내용</SmallTitle>
+
+      <textarea
+        className="w-full border-[1px] border-lightGray rounded-basic p-[5.5px] pt-2 pl-3 pr-3 text-[12px] font-Regular h-[150px] mt-3"
+        placeholder="내용을 작성해주세요."
+        value={contentValue}
+        onChange={contentInputHeight}
+      />
+    </>
+  );
 };
 
 export default WriteJokbo;
