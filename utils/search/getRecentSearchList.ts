fc7b6f20ab86d1e645d2468defa5398f68a5bbed
pakxe <pigkill40@naver.com>
2023-09-01 22:13:33 +0900

    [MATDORI-107] fix: 최근 검색어가 없을 경우 수정

--- utils/search/getRecentSearchList.ts
+++ utils/search/getRecentSearchList.ts
@@ -1,7 +1,12 @@
 export function getRecentSearchList() {
+	let resentSearchList;
+
 	if (typeof window !== 'undefined') {
-		return localStorage.getItem('recent-search-list') === null
-			? []
-			: JSON.parse(localStorage.getItem('recent-search-list') || '');
+		resentSearchList =
+			localStorage.getItem('recent-search-list') === null
+				? []
+				: JSON.parse(localStorage.getItem('recent-search-list') || '');
 	}
+
+	return resentSearchList;
 }
