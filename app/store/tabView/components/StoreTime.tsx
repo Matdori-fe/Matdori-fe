77c435e3c5871a42a9597b1ffa0d91e4eb983e4a
pakxe <pigkill40@naver.com>
2023-09-02 18:21:38 +0900

    [MATDORI-108] style: 확장 버튼 아예 오른쪽으로 밀기

--- app/store/tabView/components/StoreTime.tsx
+++ app/store/tabView/components/StoreTime.tsx
@@ -3,84 +3,80 @@ import DayTimeText from './DayTimeText';
 import { useState } from 'react';
 import { RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri';
 type StoreTimeArr = {
-  monday: string;
-  tuesday: string;
-  wednesday: string;
-  thursday: string;
-  friday: string;
-  saturday: string;
-  sunday: string;
+	monday: string;
+	tuesday: string;
+	wednesday: string;
+	thursday: string;
+	friday: string;
+	saturday: string;
+	sunday: string;
 };
 type StoreTimeProps = {
-  storeTimeArr: StoreTimeArr; // StoreTimeArr 타입을 사용
+	storeTimeArr: StoreTimeArr; // StoreTimeArr 타입을 사용
 };
 
 type Day =
-  | 'monday'
-  | 'tuesday'
-  | 'wednesday'
-  | 'thursday'
-  | 'friday'
-  | 'saturday'
-  | 'sunday';
+	| 'monday'
+	| 'tuesday'
+	| 'wednesday'
+	| 'thursday'
+	| 'friday'
+	| 'saturday'
+	| 'sunday';
 
 const daysOfWeek: Day[] = [
-  'sunday',
-  'monday',
-  'tuesday',
-  'wednesday',
-  'thursday',
-  'friday',
-  'saturday',
+	'sunday',
+	'monday',
+	'tuesday',
+	'wednesday',
+	'thursday',
+	'friday',
+	'saturday',
 ];
 
 const StoreTime: React.FC<StoreTimeProps> = ({ storeTimeArr }) => {
-  const [isDetail, setIsDetail] = useState(false);
-  const detailBtnFun = () => {
-    setIsDetail(!isDetail);
-  };
-  //오늘 날짜를 알기 위한 코드
-  const today = new Date();
-  const dayIndex = today.getDay();
-  const todayDayOfWeek: Day = daysOfWeek[dayIndex];
+	const [isDetail, setIsDetail] = useState(false);
+	const detailBtnFun = () => {
+		setIsDetail(!isDetail);
+	};
+	//오늘 날짜를 알기 위한 코드
+	const today = new Date();
+	const dayIndex = today.getDay();
+	const todayDayOfWeek: Day = daysOfWeek[dayIndex];
 
-  return (
-    <div className="w-full flex">
-      <div className="w-auto h-auto flex flex-wrap ">
-        {isDetail ? (
-          <>
-            <div className="flex">
-              <DayTimeText day="monday" time={storeTimeArr.monday} />
-              <RiArrowUpSLine
-                className="w-[16px] ml-1"
-                onClick={detailBtnFun}
-              />
-            </div>
-            <DayTimeText day="tuesday" time={storeTimeArr.tuesday} />
-            <DayTimeText day="wednesday" time={storeTimeArr.wednesday} />
-            <DayTimeText day="thursday" time={storeTimeArr.thursday} />
-            <DayTimeText day="friday" time={storeTimeArr.friday} />
-            <DayTimeText day="saturday" time={storeTimeArr.saturday} />
-            <DayTimeText day="sunday" time={storeTimeArr.sunday} />
-          </>
-        ) : (
-          <>
-            {' '}
-            <div className="flex">
-              <DayTimeText
-                day={todayDayOfWeek}
-                time={storeTimeArr[todayDayOfWeek]}
-              />
-              <RiArrowDownSLine
-                className="w-[16px] ml-1"
-                onClick={detailBtnFun}
-              />
-            </div>
-          </>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex w-full' onClick={detailBtnFun}>
+			<div className='flex flex-wrap w-auto w-full h-auto'>
+				{isDetail ? (
+					<>
+						<div className='flex justify-between w-full'>
+							<DayTimeText day='monday' time={storeTimeArr.monday} />
+							<RiArrowUpSLine className='w-[16px] ml-1' />
+						</div>
+						<DayTimeText day='tuesday' time={storeTimeArr.tuesday} />
+						<DayTimeText day='wednesday' time={storeTimeArr.wednesday} />
+						<DayTimeText day='thursday' time={storeTimeArr.thursday} />
+						<DayTimeText day='friday' time={storeTimeArr.friday} />
+						<DayTimeText day='saturday' time={storeTimeArr.saturday} />
+						<DayTimeText day='sunday' time={storeTimeArr.sunday} />
+					</>
+				) : (
+					<>
+						<div className='flex justify-between w-full'>
+							<DayTimeText
+								day={todayDayOfWeek}
+								time={storeTimeArr[todayDayOfWeek]}
+							/>
+							<RiArrowDownSLine
+								className='w-[16px] ml-1'
+								onClick={detailBtnFun}
+							/>
+						</div>
+					</>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default StoreTime;
