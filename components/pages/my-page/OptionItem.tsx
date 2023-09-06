6881cfaa0b581b43fce6e73d0429f662fda54619
pakxe <pigkill40@naver.com>
2023-09-06 20:39:23 +0900

    [MATDORI-109] refactor: mypage의 옵션 리스트가 새로운 기능 추가에 유연하도록 수정

--- components/pages/my-page/OptionItem.tsx
+++ components/pages/my-page/OptionItem.tsx
@@ -7,24 +7,96 @@ import HorizonBar from '@/components/HorizonBar/HorizonBar';
 import router from 'next/router';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import {
+	RiArticleLine,
+	RiBillLine,
+	RiCustomerServiceLine,
+	RiGroupLine,
+	RiListOrdered,
+	RiListUnordered,
+	RiLoginBoxLine,
+} from 'react-icons/ri';
+import { useInviteFriend } from '@/hooks/kakao/useInviteFriend';
+import Test from './Test';
+import ShareFriend from './ShareFriend/ShareFriend';
+import Logout from './Logout';
 
-export default function OptionItem(item: IOption) {
-	const router = useRouter();
-
-	const onClick = () => {
-		if (item.href) router.push(item.href);
-		else item.onClick;
+type OptionList = {
+	[key in IOption]: {
+		name: string;
+		icon: JSX.Element;
+		href?: string;
+		ClickAction?: ({ children }: { children: React.ReactNode }) => JSX.Element;
 	};
+};
 
-	return (
-		<div onClick={onClick}>
+export const options: OptionList = {
+	notice: {
+		name: '공지사항',
+		icon: <RiArticleLine />,
+		href: '/notice',
+	},
+	qna: {
+		name: 'FAQ',
+		icon: <RiCustomerServiceLine />,
+		href: '/qna',
+	},
+	invite: {
+		name: '친구 초대',
+		icon: <RiGroupLine />,
+		ClickAction: ShareFriend,
+	},
+	termsOfService: {
+		name: '이용약관',
+		icon: <RiListOrdered />,
+		href: '/terms-of-service',
+	},
+	privacyPolicy: {
+		name: '개인정보 처리방침',
+		icon: <RiListUnordered />,
+		href: '/privacy-policy',
+	},
+	logout: {
+		name: '로그아웃',
+		icon: <RiLoginBoxLine />,
+		ClickAction: Logout,
+	},
+	version: {
+		name: '버전 정보',
+		icon: <RiBillLine />,
+		href: '/version',
+	},
+};
+
+export default function OptionItem({ variant }: { variant: IOption }) {
+	const innerComponent = (
+		<div>
 			<div className='h-[48px] flex items-center gap-[10px] px-[10px]'>
-				<div className='text-sm text-100'>{item.icon}</div>
+				<div className='text-sm text-100'>{options[variant].icon}</div>
 				<Text size='sm' fontWeight='semibold'>
-					{item.name}
+					{options[variant].name}
 				</Text>
 			</div>
 			<div className='h-[1px] bg-lightGray w-full' />
 		</div>
 	);
+
+	// NOTE: onClick만 인자로 쓰기엔 훅을 인자로 넣을 수 없어서 어쩔 수 없이 이렇게 분리.
+	if (options[variant].ClickAction) {
+		const ClickAction = options[variant].ClickAction as ({
+			children,
+		}: {
+			children: React.ReactNode;
+		}) => JSX.Element;
+
+		return <ClickAction>{innerComponent}</ClickAction>;
+	}
+
+	if (options[variant].href) {
+		const href = options[variant].href as string;
+
+		return <Link href={href}>{innerComponent}</Link>;
+	}
+
+	return innerComponent;
 }
--- components/pages/my-page/OptionSection.tsx
+++ components/pages/my-page/OptionSection.tsx
@@ -1,68 +1,35 @@
 import Text from '@/components/Text/Text';
-import {
-  RiArticleLine,
-  RiBillLine,
-  RiCustomerServiceLine,
-  RiGroupLine,
-  RiListOrdered,
-  RiListUnordered,
-  RiLoginBoxLine,
-} from 'react-icons/ri';
+
 import OptionItem from './OptionItem';
 import ShareFriend from './ShareFriend/ShareFriend';
 
 // REFACTOR: 굳이 href 방식이 아닌 다른 방식으로 해도될 것 같음. 불필요한 페이지가 만들어지기 때문임
-export const options = [
-  {
-    name: '공지사항',
-    icon: <RiArticleLine />,
-    href: '/notice',
-  },
-  {
-    name: 'FAQ',
-    icon: <RiCustomerServiceLine />,
-    href: '/qna',
-  },
-  {
-    name: '친구 초대',
-    icon: <RiGroupLine />,
-  },
-  {
-    name: '이용약관',
-    icon: <RiListOrdered />,
-    href: '/terms-of-service',
-  },
-  {
-    name: '개인정보 처리방침',
-    icon: <RiListUnordered />,
-    href: '/privacy-policy',
-  },
-  {
-    name: '로그아웃',
-    icon: <RiLoginBoxLine />,
-    href: '/logout',
-  },
-  {
-    name: '버전 정보',
-    icon: <RiBillLine />,
-    href: '/version',
-  },
+
+const optionList: IOption[] = [
+	'notice',
+	'qna',
+	'invite',
+	'termsOfService',
+	'privacyPolicy',
+	'logout',
+	'version',
 ];
 
-export interface IOption {
-  name: string;
-  icon: JSX.Element;
-  href?: string;
-  onClick?: JSX.Element;
-}
+export type IOption =
+	| 'notice'
+	| 'qna'
+	| 'invite'
+	| 'termsOfService'
+	| 'privacyPolicy'
+	| 'logout'
+	| 'version';
 
 export default function OptionSection() {
-  return (
-    <>
-      {options.map((item: IOption) => (
-        <OptionItem {...item} key={item.name} />
-      ))}
-      <ShareFriend />
-    </>
-  );
+	return (
+		<>
+			{optionList.map((variant) => (
+				<OptionItem variant={variant} key={variant} />
+			))}
+		</>
+	);
 }
