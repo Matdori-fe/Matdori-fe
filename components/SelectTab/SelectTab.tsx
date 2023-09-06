2da6e9fe6f4254d0e447b3ac1e800527a7b78add
pakxe <pigkill40@naver.com>
2023-09-06 19:42:08 +0900

    [MATDORI-109] feat: tab 변경시 underline이 이동하는 애니메이션 추가

--- components/SelectTab/SelectTab.tsx
+++ components/SelectTab/SelectTab.tsx
@@ -18,6 +18,10 @@ const Underline = styled(motion.div)<{
 }>`
 	@media (min-width: 412px) {
 		width: calc(412px / ${({ listLength }) => listLength});
+		animation-duration: 3s;
+		animation-iteration-count: 2;
+		animation-timing-function: ease-in;
+		animation-direction: normal;
 	}
 `;
 
@@ -35,17 +39,36 @@ export default function SelectTab({ type = 'none' }: ISelectTab) {
 	const sectionList = useRecoilValue(SelectTabAtom(tab));
 	const bo = useRef(null);
 
+	console.log(section);
+
+	const animateUnderLine = () => {
+		if (sectionList.length === 2)
+			return sectionList[0].id === section ? 0 : '100%';
+		// 탭의 아이템 개수가 3개인 경우
+		else {
+			if (sectionList[0].id === section) return 0;
+			if (sectionList[1].id === section) return '100%';
+			if (sectionList[2].id === section) return '200%';
+		}
+	};
+
+	const underlineWidth = () => {
+		if (sectionList.length === 2) return 'w-[calc(50%)]';
+		// 탭의 아이템 개수가 3개인 경우
+		else return 'w-[calc(33.3%)]';
+	};
+
 	return (
-		<div
+		<motion.div
 			className={
 				type === 'none'
 					? 'z-10 sm:w-[412px] w-full border-b-[2px] py-[13px] border-gray bg-white'
 					: 'z-10 sm:w-[412px] w-full border-b-[2px] py-[13px] border-gray bg-white fixed top-0'
 			}
 		>
-			<motion.ul className='flex'>
+			<motion.ul className='relative flex overflow-hidden'>
 				{sectionList.map(({ id, name }) => (
-					<li
+					<motion.li
 						key={id}
 						className='relative flex justify-center grow'
 						onClick={() =>
@@ -59,19 +82,24 @@ export default function SelectTab({ type = 'none' }: ISelectTab) {
 						>
 							{name}
 						</Text>
-						{section === id && (
-							<Underline
-								key={id}
-								listLength={sectionList.length}
-								// layoutId='underline'
-								className='absolute border-b-[2px] border-100 top-[34px] w-full'
-							/>
-						)}
-					</li>
+						{/* {section === id && ( */}
+
+						{/* )} */}
+					</motion.li>
 				))}
 			</motion.ul>
-		</div>
+			<Underline
+				transition={{ type: 'spring', duration: 0.5 }}
+				// transition={{ type: 'inertia', velocity: 50 }}
+				animate={{ x: animateUnderLine() }}
+				listLength={sectionList.length}
+				className={`absolute border-b-[2px] border-100 ${underlineWidth()} bottom-[-2px]`}
+			/>
+		</motion.div>
 	);
 }
 
 // FIXME: framer motion에서 path변경시 모션이 이상하게 동작함
+// ${
+// 	sectionList[0].id === section ? 'left-0' : 'right-0'
+// }
