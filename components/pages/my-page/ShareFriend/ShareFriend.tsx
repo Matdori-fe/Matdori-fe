ed206082910a0aab33d873c4ff82c0417dcb45ee
pakxe <pigkill40@naver.com>
2023-09-06 20:38:32 +0900

    [MATDORI-109] feat: children을 받아 감싸는 클릭 컴포넌트가 될 수 있도록 수정함

--- components/pages/my-page/ShareFriend/ShareFriend.tsx
+++ components/pages/my-page/ShareFriend/ShareFriend.tsx
@@ -2,48 +2,46 @@
 import { useEffect, useState } from 'react';
 
 declare global {
-  interface Window {
-    Kakao: any;
-  }
+	interface Window {
+		Kakao: any;
+	}
 }
 
-const ShareFriend = () => {
-  const [isKakaoLoaded, setIsKakaoLoaded] = useState(false);
-  const { Kakao, location } = window;
+const ShareFriend = ({ children }: { children: React.ReactNode }) => {
+	const [isKakaoLoaded, setIsKakaoLoaded] = useState(false);
+	const { Kakao, location } = window;
 
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://developers.kakao.com/sdk/js/kakao.min.js';
-    script.defer = true;
-    script.onload = () => {
-      setIsKakaoLoaded(true);
-    };
-    document.head.appendChild(script);
+	useEffect(() => {
+		const script = document.createElement('script');
+		script.src = 'https://developers.kakao.com/sdk/js/kakao.min.js';
+		script.defer = true;
+		script.onload = () => {
+			setIsKakaoLoaded(true);
+		};
+		document.head.appendChild(script);
 
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
+		return () => {
+			document.head.removeChild(script);
+		};
+	}, []);
 
-  useEffect(() => {
-    if (isKakaoLoaded) {
-      Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
-    }
-  }, [isKakaoLoaded]);
+	useEffect(() => {
+		if (isKakaoLoaded) {
+			Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+		}
+	}, [isKakaoLoaded]);
 
-  const onClick = () => {
-    if (isKakaoLoaded) {
-      Kakao.Link.sendScrap({
-        requestUrl: location.href,
-        templateId: 97992,
-        templateArgs: {},
-      });
-    }
-  };
+	const onClick = () => {
+		if (isKakaoLoaded) {
+			Kakao.Link.sendScrap({
+				requestUrl: location.href,
+				templateId: 97992,
+				templateArgs: {},
+			});
+		}
+	};
 
-  const ShareFriendBtn = () => <div onClick={onClick}>친구초대</div>;
-
-  return <ShareFriendBtn />;
+	return <div onClick={onClick}>{children}</div>;
 };
 
 export default ShareFriend;
