ba900c190a8825125cbeffda5b408c98f6c23570
pakxe <pigkill40@naver.com>
2023-09-06 16:23:07 +0900

    [MATDORI-109] chore: modal 사용법 저장

--- app/view/home/page.tsx
+++ app/view/home/page.tsx
@@ -1,18 +1,33 @@
+'use client';
+
 import HorizonBar from '@/components/HorizonBar/HorizonBar';
-import ModalContainer from '@/components/ModalContainer/ModalContainer';
+import ModalContainer, {
+	modals,
+} from '@/components/ModalContainer/ModalContainer';
 import DepartmentModalOpener from '@/components/ModalOpener/DepartmentModalOpener';
 import CarouselSection from '@/components/pages/home/CarouselSection';
 import CategorySection from '@/components/pages/home/CategorySection';
 import InfiniteShopListSection from '@/components/pages/home/InfiniteShopListSection';
 import RecommendButtonsSection from '@/components/pages/home/RecommendButtonsSection';
 import Top3Section from '@/components/pages/home/Top3Section';
+import { useModal } from '@/hooks/useModal';
 import axios from 'axios';
 
 export default function Home() {
+	// 1. 모달을 열기위한 훅을 호출한다.
+	const { openModal } = useModal();
+
 	return (
 		<div className='flex flex-col gap-[30px]'>
 			<CarouselSection />
+
 			<RecommendButtonsSection />
+
+			{/* 2. 호출한 훅에 id와 모달 컴포넌트를 담는다. 모달 컴포넌트는 modals를 import해와서 원하는 모달을 .연산자로 선택  */}
+			<button onClick={() => openModal({ id: 'more', Component: modals.more })}>
+				modal
+			</button>
+
 			<CategorySection />
 			<Top3Section />
 			<div className='mb-[100px]' />
--- app/view/layout.tsx
+++ app/view/layout.tsx
@@ -1,15 +1,17 @@
 import ModalContainer from '@/components/ModalContainer/ModalContainer';
 import NavigationBar from '@/components/NavigationBar/NavigationBar';
 export default function NavigationLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <>
-      <div className="mx-[20px]">{children}</div>
-      <NavigationBar />
-      <ModalContainer />
-    </>
-  );
+	return (
+		<>
+			<div className='mx-[20px]'>{children}</div>
+			<NavigationBar />
+
+			{/* 3. 사용하고자 하는 페이지에 layout에 ModalContainer 삽입. (실제 모달을 띄워주는 컴포넌트) */}
+			<ModalContainer />
+		</>
+	);
 }
