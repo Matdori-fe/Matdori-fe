2f96e9c39068ab365c850c5e7e0a7c35589feab2
pakxe <pigkill40@naver.com>
2023-09-07 15:44:31 +0900

    [MATDORI-109] chore: 사용법 작성했던 코드 삭제

--- app/view/home/page.tsx
+++ app/view/home/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import HorizonBar from '@/components/HorizonBar/HorizonBar';
 import ModalContainer, {
 	modals,
@@ -14,19 +12,11 @@ import { useModal } from '@/hooks/useModal';
 import axios from 'axios';
 
 export default function Home() {
-	// 1. 모달을 열기위한 훅을 호출한다.
-	const { openModal } = useModal();
-
 	return (
 		<div className='flex flex-col gap-[30px]'>
 			<CarouselSection />
 
 			<RecommendButtonsSection />
-
-			{/* 2. 호출한 훅에 id와 모달 컴포넌트를 담는다. 모달 컴포넌트는 modals를 import해와서 원하는 모달을 .연산자로 선택  */}
-			<button onClick={() => openModal({ id: 'more', Component: modals.more })}>
-				modal
-			</button>
 
 			<CategorySection />
 			<Top3Section />
--- app/view/layout.tsx
+++ app/view/layout.tsx
@@ -10,7 +10,6 @@ export default function NavigationLayout({
 			<div className='mx-[20px]'>{children}</div>
 			<NavigationBar />
 
-			{/* 3. 사용하고자 하는 페이지에 layout에 ModalContainer 삽입. (실제 모달을 띄워주는 컴포넌트) */}
 			<ModalContainer />
 		</>
 	);
