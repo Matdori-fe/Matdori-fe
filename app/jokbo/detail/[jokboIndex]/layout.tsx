2b2bbff10798bda403bd6ae2025f3ee61f1e8384
sean <sean2337@naver.com>
2023-09-12 18:11:51 +0900

    [MATDORI-110] feat: 족보, 댓글 삭제 모달 연결

--- app/jokbo/detail/[jokboIndex]/layout.tsx
+++ app/jokbo/detail/[jokboIndex]/layout.tsx
@@ -1,7 +1,9 @@
+import ModalContainer from '@/components/ModalContainer/ModalContainer';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="px-[20px]">{children}</div>
+      <ModalContainer />
     </>
   );
 }
--- components/HeaderComponents/More.tsx
+++ components/HeaderComponents/More.tsx
@@ -1,74 +1,24 @@
-import { useState } from 'react';
+'use client';
 import { RiMore2Fill } from 'react-icons/ri';
 import { DeleteType } from './type/HeaderComponentsType';
-import axios from 'axios';
-import { useRecoilValue } from 'recoil';
-import { UserAtom } from '@/atoms/UserAtom';
-import Toast from '../Toast/Toast';
 import { useRouter } from 'next/navigation';
+import { useModal } from '@/hooks/useModal';
+import { modals } from '@/components/ModalContainer/ModalContainer';
+
 const More = ({ id, kind }: DeleteType) => {
-  const [showOptions, setShowOptions] = useState(false);
-  const user = useRecoilValue(UserAtom);
   const router = useRouter();
 
-  const handleOptionChange = (e: any) => {
-    const selectedOption = e.target.value;
-    if (selectedOption === 'delete') {
-      if (kind === 'jokbo') {
-        axios
-          .post(
-            `${process.env.NEXT_PUBLIC_API}/users/${user.userId}/jokbos`,
-            {
-              jokboIdList: [id],
-            },
-            {
-              withCredentials: true,
-            }
-          )
-          .then((response) => {
-            console.log(response);
-            if (response.status === 200) {
-              Toast('삭제가 완료되었습니다.');
-              router.back();
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-      if (kind === 'comment') {
-        axios
-          .post(
-            `${process.env.NEXT_PUBLIC_API}/users/${user.userId}/comments`,
-            {
-              jokboCommentIdList: [id],
-            },
-            {
-              withCredentials: true,
-            }
-          )
-          .then((response) => {
-            console.log(response);
-            if (response.status === 200) {
-              Toast('삭제가 완료되었습니다.');
-              window.location.reload();
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    }
-    // Close the options
-    setShowOptions(false);
-  };
+  const { openModal } = useModal();
 
   return (
     <div className="relative inline-block">
       {kind === 'jokbo' && (
         <RiMore2Fill
           size="20"
-          onClick={() => setShowOptions(!showOptions)}
+          onClick={() => {
+            router.replace(`?kind=${kind}&id=${id}`);
+            openModal({ id: 'more', Component: modals.more });
+          }}
           className="cursor-pointer"
         />
       )}
@@ -76,23 +26,12 @@ const More = ({ id, kind }: DeleteType) => {
       {kind === 'comment' && (
         <RiMore2Fill
           size="12"
-          onClick={() => setShowOptions(!showOptions)}
+          onClick={() => {
+            router.replace(`?kind=${kind}&id=${id}`);
+            openModal({ id: 'more', Component: modals.more });
+          }}
           className="cursor-pointer"
         />
-      )}
-
-      {showOptions && (
-        <ul className="absolute mt-2 space-y-2 bg-white border border-gray-200 py-2 px-4 rounded shadow-lg ">
-          <li>
-            <button
-              value="delete"
-              onClick={handleOptionChange}
-              className="text-100 hover:text-100 text-[12px] whitespace-nowrap"
-            >
-              삭제
-            </button>
-          </li>
-        </ul>
       )}
     </div>
   );
